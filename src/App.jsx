4fd3c23af56252d779cf50df294707f16cd0ddc0
unknown <jhonerix23@gmail.com>
2025-02-10 10:42:35 +0800

    removed comments in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { Routes } from "react-router";
 import ClientRoute from "./routes/client/ClientRoute";
 import LandingPageRoute from "./routes/landingPage/LandingPageRoute";
-import PageNotFoundRoute from "./routes/PageNotFoundRoute";
 
 function App() {
   return (
     <>
       <LandingPageRoute />
       <ClientRoute />
-      {/* <PageNotFoundRoute /> */}
     </>
   );
 }
