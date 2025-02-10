7ae70ac20e0117a2ad44e37df00ae737396599a3
Bianca Marie Victorino <biancamarie.victorino@gmail.com>
2025-02-10 10:57:55 +0800

    modified login.jsx from dev branch

--- src/pages/landingPage/Login.jsx
+++ src/pages/landingPage/Login.jsx
@@ -1,7 +1,7 @@
 const Login = () => {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <h1>This is the Login Page</h1>
+      <h1>This is the Login Page from dev branch</h1>
     </div>
   );
 };
