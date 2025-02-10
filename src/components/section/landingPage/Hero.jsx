8ed5785a9d6f32615d96dc843a5f4e99ddbb8827
unknown <jhonerix23@gmail.com>
2025-02-10 15:46:25 +0800

    added hero content

--- src/components/section/landingPage/Hero.jsx
+++ src/components/section/landingPage/Hero.jsx
@@ -1,5 +1,9 @@
 const Hero = () => {
-  return <section></section>;
+  return (
+    <section className="flex justify-center items-center min-h-screen">
+      <h1>THIS IS LANDING PAGE</h1>
+    </section>
+  );
 };
 
 export default Hero;
