7cfa4a76e8e835fee7def73fdc1e60e4fbdc0792
unknown <jhonerix23@gmail.com>
2025-02-10 13:33:26 +0800

    added theme variable for primary color

--- src/pages/landingPage/Home.jsx
+++ src/pages/landingPage/Home.jsx
@@ -1,9 +1,7 @@
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <h1>
-        
-      </h1>
+      <h1 className="text-primary-color">Hello World</h1>
     </div>
   );
 };
