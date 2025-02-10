8d6f327d8ecc68719a315a4ed53bb67ad1cc2739
unknown <jhonerix23@gmail.com>
2025-02-10 15:23:23 +0800

    fixed navbar

--- src/components/section/landingPage/Hero.jsx
+++ src/components/section/landingPage/Hero.jsx
@@ -1,23 +1,5 @@
-import Navbar from "../../navbar/Navbar";
-import DecorationTop from "../../../assets/images/icon/decoration-top-hero.png";
-import DecorationBottom from "../../../assets/images/icon/decoration-bottom-hero.png";
-
 const Hero = () => {
-  return (
-    <section>
-      <Navbar />
-      <img
-        src={DecorationTop}
-        alt="Decoration Top"
-        className="hidden lg:block fixed top-0 left-0 -z-10"
-      />
-      <img
-        src={DecorationBottom}
-        alt="Decoration Bottom"
-        className="hidden lg:block fixed bottom-0 right-0 -z-10"
-      />
-    </section>
-  );
+  return <section></section>;
 };
 
 export default Hero;
--- src/pages/landingPage/Home.jsx
+++ src/pages/landingPage/Home.jsx
@@ -1,9 +1,23 @@
 import Hero from "../../components/section/landingPage/Hero";
+import Navbar from "../../components/navbar/Navbar";
+import DecorationTop from "../../assets/images/icon/decoration-top-hero.png";
+import DecorationBottom from "../../assets/images/icon/decoration-bottom-hero.png";
 
 const Home = () => {
   return (
     <>
+      <Navbar />
       <Hero />
+      <img
+        src={DecorationTop}
+        alt="Decoration Top"
+        className="hidden lg:block fixed top-0 left-0 -z-10"
+      />
+      <img
+        src={DecorationBottom}
+        alt="Decoration Bottom"
+        className="hidden lg:block fixed bottom-0 right-0 -z-10"
+      />
     </>
   );
 };
