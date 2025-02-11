ccb2edb5ed05366bb8166d5507773298d1d7867d
unknown <jhonerix23@gmail.com>
2025-02-11 11:17:49 +0800

    added tickets tab in client

--- src/pages/client/Tickets.jsx
+++ src/pages/client/Tickets.jsx
@@ -1,14 +1,39 @@
 import React from "react";
+import { Tabs } from "antd";
 
 const Tickets = () => {
   return (
-    <div className="grid grid-cols-3 gap-4">
-      {Array.from({ length: 50 }, (_, index) => (
-        <div key={index} className="px-4 py-10 border border-red-600 text-xs md:text-sm">
-          Sample Ticket No.{index + 1}
-        </div>
-      ))}
-    </div>
+    <Tabs
+      type="card"
+      items={[
+        {
+          key: 1,
+          label: "UNUSED",
+          children: (
+            <div className="grid grid-cols-3 gap-4">
+              {Array.from({ length: 50 }, (_, index) => (
+                <div key={index} className="px-4 py-10 border border-red-600 text-xs md:text-sm">
+                  Sample UNUSED TICKETS No.{index + 1}
+                </div>
+              ))}
+            </div>
+          ),
+        },
+        {
+          key: 2,
+          label: "EXPIRED",
+          children: (
+            <div className="grid grid-cols-3 gap-4">
+              {Array.from({ length: 50 }, (_, index) => (
+                <div key={index} className="px-4 py-10 border border-red-600 text-xs md:text-sm">
+                  Sample EXPIRED TICKETS No.{index + 1}
+                </div>
+              ))}
+            </div>
+          ),
+        },
+      ]}
+    />
   );
 };
 
