6430adc14dea3859e9e0b51b29f31604a6e89f38
unknown <jhonerix23@gmail.com>
2025-02-11 11:41:24 +0800

    modified organizers images size

--- src/components/section/landingPage/Hero.jsx
+++ src/components/section/landingPage/Hero.jsx
@@ -12,20 +12,13 @@ const Hero = () => {
     <section className="lg:px-36 lg:pt-12 lg:h-screen lg:overflow-hidden lg:grid lg:grid-cols-4 lg:grid-rows-3 lg:gap-4">
       <div className="lg:justify-start lg:col-span-2 lg:h-full lg:row-span-2 lg:mt-16 px-8 flex flex-col justify-between min-h-screen">
         <div className="flex flex-col gap-4">
-          <img
-            src={HeaderLogo}
-            className="py-8 lg:hidden pt-20"
-            alt="WorldBex Logo"
-          />
-          <h1
-            style={{ color: token.colorPrimary }}
-            className="text-3xl font-bold"
-          >
+          <img src={HeaderLogo} className="py-8 lg:hidden pt-20" alt="WorldBex Logo" />
+          <h1 style={{ color: token.colorPrimary }} className="text-3xl font-bold">
             WELCOME TO WORLDBEX SERVICES INTERNATIONAL
           </h1>
           <p className="text-md text-gray-700 font-normal">
-            THE LEADING EXHIBITION AND EVENTS MANAGEMENT COMPANY IN THE
-            PHILIPPINES WITH OVER 25 YEARS OF EXCELLENCE
+            THE LEADING EXHIBITION AND EVENTS MANAGEMENT COMPANY IN THE PHILIPPINES WITH OVER 25
+            YEARS OF EXCELLENCE
           </p>
         </div>
 
@@ -49,9 +42,7 @@ const Hero = () => {
       {/* Organizations */}
       <div className="p-8 lg:p-0 lg:col-span-2 lg:col-start-1 lg:row-start-3">
         <hr className="border-gray-300 lg:hidden" />
-        <h1 className="text-gray-500 text-center p-4 lg:p-2 lg:text-sm">
-          Organizer Of
-        </h1>
+        <h1 className="text-gray-500 text-center p-4 lg:p-2 lg:text-sm">Organizer Of</h1>
         <div className="grid grid-cols-2 place-items-center lg:grid-rows-2 lg:grid-cols-7">
           {organizers.map((org, index) => (
             <div key={index} className="p-2 shrink-0 md:shrink-1">
@@ -59,7 +50,7 @@ const Hero = () => {
                 src={org.image}
                 alt={org.alt}
                 loading="lazy"
-                className="hover:scale-150 duration-300 lg:max-w-[40px]"
+                className="hover:scale-150 duration-300"
               />
             </div>
           ))}
