0e2080f0f9f09c1c8940ef7527f0567898e590b0
unknown <jhonerix23@gmail.com>
2025-02-12 09:45:28 +0800

    new hero layout

--- src/components/section/landingPage/Hero.jsx
+++ src/components/section/landingPage/Hero.jsx
@@ -9,56 +9,68 @@ const Hero = () => {
   const { token } = theme.useToken();
 
   return (
-    <section className="xl:px-36 xl:pt-12 xl:h-screen xl:overflow-hidden xl:grid xl:grid-cols-4 xl:grid-rows-3 lg:gap-4">
-      <div className="md:justify-center xl:justify-start xl:col-span-2 xl:h-full xl:row-span-2 xl:mt-16 px-8 flex flex-col justify-between min-h-screen">
-        <div className="flex flex-col gap-4">
-          <img src={HeaderLogo} className="py-8 xl:hidden pt-20 md:hidden" alt="WorldBex Logo" />
-          <h1 style={{ color: token.colorPrimary }} className="text-3xl font-bold">
-            WELCOME TO WORLDBEX SERVICES INTERNATIONAL
-          </h1>
-          <p className="text-md text-gray-700 font-normal">
-            THE LEADING EXHIBITION AND EVENTS MANAGEMENT COMPANY IN THE PHILIPPINES WITH OVER 25
-            YEARS OF EXCELLENCE
-          </p>
-        </div>
-
-        {/* Buttons */}
-        <div className="flex flex-row w-full justify-between gap-3 pt-16 pb-6">
-          <div className="flex flex-col w-full gap-3 text-[#F4811F] font-semibold">
-            <h1 className="hidden xl:flex">Not Yet Registered?</h1>
-            <Button type="primary" size="large">
-              Register
-            </Button>
+    <section className="xl:flex xl:items-center xl:px-20">
+      <div className="xl:mt-10 xl:w-[60%] lg:mt-14">
+        <div className="h-screen flex flex-col lg:gap-10 justify-between py-6 px-4 md:justify-around md:px-20 lg:min-h-screen">
+          <div className="flex flex-col gap-6 mt-14">
+            <img src={HeaderLogo} alt="worldbex Logo" className="lg:hidden" />
+            <h1 className="text-primary-color text-2xl font-bold leading-9 md:text-4xl md:leading-14 lg:text-6xl lg:leading-22 xl:text-4xl xl:leading-12">
+              WELCOME TO WORLDBEX SERVICES INTERNATIONAL
+            </h1>
+            <p className="md:text-xl leading-7 md:leading-9 lg:text-2xl lg:leading-12 xl:text-xl xl:leading-9 font-medium">
+              THE LEADING EXHIBITION AND EVENTS MANAGEMENT COMPANY IN THE PHILIPPINES WITH OVER 25
+              YEARS OF EXCELLENCE
+            </p>
           </div>
-          <div className="flex flex-col w-full gap-3 text-[#F4811F] font-semibold">
-            <h1 className="hidden xl:flex">Already Registered?</h1>
-            <Button type="primary" size="large">
-              Sign In
-            </Button>
-          </div>
-        </div>
-      </div>
-
-      {/* Organizations */}
-      <div className="p-8 xl:p-0 xl:col-span-2 xl:col-start-1 xl:row-start-3">
-        <hr className="border-gray-300 xl:hidden" />
-        <h1 className="text-gray-500 text-center p-4 lg:p-2 lg:text-sm">Organizer Of</h1>
-        <div className="grid grid-cols-2 place-items-center xl:grid-rows-2 xl:grid-cols-7">
-          {organizers.map((org, index) => (
-            <div key={index} className="p-2 shrink-0 md:shrink-1">
-              <img
-                src={org.image}
-                alt={org.alt}
-                loading="lazy"
-                className="hover:scale-150 duration-300"
-              />
+          <div>
+            <div className="hidden lg:block shrink-0 xl:hidden xl:w-[40%]">
+              <img src={HeroImage} alt="Hero Image" loading="lazy" />
             </div>
-          ))}
+          </div>
+          <div className="flex justify-center items-center gap-2 md:gap-10 lg:gap-20 xl:gap-30">
+            <div className="flex flex-col gap-3 w-full font-medium">
+              <h1 className="text-primary-color hidden xl:block">Not yet registered?</h1>
+              <Button type="primary" size="large">
+                REGISTER
+              </Button>
+            </div>
+            <div className="flex flex-col gap-3 w-full font-medium">
+              <h1 className="text-primary-color hidden xl:block">Already registered?</h1>
+              <Button type="primary" size="large">
+                SIGN IN
+              </Button>
+            </div>
+          </div>
+          <div className="py-10 hidden lg:block w-full">
+            <h1 className="text-center mb-4 font-medium">Organizer of</h1>
+            <div className="grid grid-cols-7 gap-2 place-items-center">
+              {organizers.map((org) => (
+                <>
+                  <img
+                    src={org.image}
+                    alt="Organizations"
+                    loading="lazy"
+                    className="hover:scale-125 duration-300"
+                  />
+                </>
+              ))}
+            </div>
+          </div>
+        </div>
+        <hr className="border-gray-200 mt-4 mb-4 m-10 lg:hidden" />
+        <div className="py-10 lg:hidden">
+          <h1 className="text-center mb-4 font-medium">Organizer of</h1>
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-2 place-items-center">
+            {organizers.map((org) => (
+              <>
+                <img src={org.image} alt="Organizations" loading="lazy" />
+              </>
+            ))}
+          </div>
         </div>
       </div>
-
-      <div className="hidden xl:place-items-center xl:grid xl:col-span-2 xl:row-span-3 xl:col-start-3 xl:row-start-1">
-        <img src={HeroImage} className="max-h-full object-cover" alt="" />
+      <div className="hidden xl:block shrink-0 xl:w-[40%]">
+        <img src={HeroImage} alt="Hero Image" loading="lazy" />
       </div>
     </section>
   );
