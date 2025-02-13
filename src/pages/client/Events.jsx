2f91f16a427daad6c541997656ee065af6648508
Bianca Marie Victorino <biancamarie.victorino@gmail.com>
2025-02-13 15:20:16 +0800

    added downloadables

--- src/pages/client/Events.jsx
+++ src/pages/client/Events.jsx
@@ -4,7 +4,10 @@ const Events = () => {
   return (
     <div className="grid grid-cols-3 gap-4">
       {Array.from({ length: 50 }, (_, index) => (
-        <div key={index} className="px-4 py-10 border border-blue-600 text-xs md:text-sm">
+        <div
+          key={index}
+          className="px-4 py-10 border border-blue-600 text-xs md:text-sm"
+        >
           Sample Event No.{index + 1}
         </div>
       ))}
